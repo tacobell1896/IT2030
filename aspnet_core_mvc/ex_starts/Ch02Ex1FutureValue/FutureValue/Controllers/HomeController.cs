f2aeb38cb71947f6c2291b5c479e8bbac5cff5bb
Turner Bell <turner@tbell.us>
2021-02-01 22:15:43 -0500

    add ch02 notes and project

---
+++ aspnet_core_mvc/ex_starts/Ch02Ex1FutureValue/FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FutureValue.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ aspnet_core_mvc/ex_starts/Ch02Ex1FutureValue/FutureValue/Models/FutureValueModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FutureValue.Models
+{
+    
+    public class FutureValueModel
+    {
+        [Required(ErrorMessage = "Please enter a monthly investment.")]
+        [Range(1, 500, ErrorMessage =
+        "Monthly investment amount must be between 1 and 500.")]
+        public decimal? MonthlyInvestment { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a yearly interest rate.")]
+        [Range(1, 500, ErrorMessage =
+        "Monthly investment amount must be between 0.1 and 10.0.")]
+        public decimal? YearlyInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a number of years")]
+        [Range(1, 500, ErrorMessage =
+        "Monthly investment amount must be between 1 and 50.")]
+        public int? Years { get; set; }
+        public decimal? CalculateFutureValue()
+        {
+            int months = 12;
+            decimal? monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            decimal? futureValue = 0;
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue;
+        }
+    }
+}
