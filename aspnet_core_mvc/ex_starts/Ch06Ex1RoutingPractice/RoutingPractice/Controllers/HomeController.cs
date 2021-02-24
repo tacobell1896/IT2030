3372de0581771e0eb8755c843a04245ef3114f6d
Turner Bell <turner@tbell.us>
2021-02-23 23:24:53 -0500

    add Ch06 exercise

--- aspnet_core_mvc/ex_starts/Ch06Ex1RoutingPractice/RoutingPractice/Controllers/HomeController.cs
+++ aspnet_core_mvc/ex_starts/Ch06Ex1RoutingPractice/RoutingPractice/Controllers/HomeController.cs
@@ -25,5 +25,17 @@ namespace RoutingPractice.Controllers
                 return Content("ID: " + id);
             }
         }
+        [Route("[action]/{start}/{end?}/{message?}")]
+        public IActionResult Countdown(int start, int end = 0, string message = "")
+        {
+            string contentString = "Counting down:\n";
+            for(int i = start; i >= end; i--)
+            {
+                contentString += i + "\n";
+                if (i == end)
+                    contentString += message;
+            }
+            return Content(contentString);
+        }
     }
 }
