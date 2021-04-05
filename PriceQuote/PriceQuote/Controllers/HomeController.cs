cf703504079dcdfd56ea148d5451ee3d417c5472
Turner Bell <turner@tbell.us>
2021-04-04 21:50:17 -0400

    Add Project 2

---
+++ PriceQuote/PriceQuote/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuote.Models;
+
+
+namespace PriceQuote.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.PriceQuote = "";
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(PriceQuoteModel pq)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.amount = pq.Amount().ToString("c2");
+                ViewBag.total = pq.Total().ToString("c2");
+            }
+            else
+            {
+                ModelState.AddModelError("","Please fix all validation errors.");
+                return View(pq);
+            }
+            return View(pq);
+        }
+    }
+}
---
+++ PriceQuote/PriceQuote/Models/PriceQuoteModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuote.Models
+{
+    public class PriceQuoteModel
+    {
+        [Required(ErrorMessage = "Please enter a total greater than zero")]
+        public decimal? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a number between 0 and 100")]
+        [Range(0, 100)]
+        public int? DiscountRate { get; set; }
+
+        public decimal Amount()
+        {
+            decimal amount = Subtotal.Value * DiscountRate.Value / 100;
+
+            return amount;
+        }
+
+        public decimal Total()
+        {
+            
+            decimal total = Subtotal.Value - Amount();
+
+            return total;
+            
+        }
+
+
+    }
+}
